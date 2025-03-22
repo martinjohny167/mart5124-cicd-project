8f251a9ce9dc3b9c0e2c0d18a0d53ce3451c9a3c
Martin <martin@example.com>
2025-03-22 18:20:49 -0400

    Initial commit

---
+++ test/cicd_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CicdProject from '../lib/cicd_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicd_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CicdProject.CicdProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
